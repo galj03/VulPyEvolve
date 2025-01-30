7301458f466ace926987bddd7b10667689852092
Gál József <h260710@stud.u-szeged.hu>
2025-01-30 14:48:22 +0100

    Planning

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from src.facades import pyevolve_facade
 
 if __name__ == '__main__':
-    # os.chdir(sys.argv[1])
+    # TODO: check whether the rules are inferred (check if patterns path is empty???)
+    # if not, then:
+    # TODO: 1. check for database
+    # (if not present, then just exit with an error (for now))
+    # TODO: 2. extract data from db
+    # TODO: 3. run run_pyevolve_infer
+
+    # TODO: if all is good, run_pyevolve_transform on the given repo
     print("res: ", pyevolve_facade.run_pyevolve_infer("/patterns", "/rules"))
     print("res: ", pyevolve_facade.run_pyevolve_transform("/repos", "/types", "/files", "/pattsies"))
