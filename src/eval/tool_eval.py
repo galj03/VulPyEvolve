74fffee30d527b96dd0373aec6a6ac4598453c00
Gál József <h260710@stud.u-szeged.hu>
2025-04-07 17:46:31 +0200

    Eval step 0

--- src/eval/tool_eval.py
+++ src/eval/tool_eval.py
@@ -1,7 +1,19 @@
+import os
+from pathlib import Path
 
+from src.config import configuration as cf
 
 # TODO: workflow:
 # 0. set config values
+ROOT_DIR = Path.cwd().parent.parent
+EVAL_ROOT_DIR = os.path.join(ROOT_DIR.parent, "_infer_data\\eval")
+
+cf.PROJECT_REPO = os.path.join(EVAL_ROOT_DIR, "project_repo")
+cf.TYPE_REPO = os.path.join(EVAL_ROOT_DIR, "type_repo")
+cf.PATTERNS_PATH = os.path.join(EVAL_ROOT_DIR, "patterns")
+cf.RULES_PATH = os.path.join(EVAL_ROOT_DIR, "rules")
+cf.FILES_PATH = os.path.join(EVAL_ROOT_DIR, "files.txt")
+
 # 1. collect all fixes from db to a temp dir
 # 2. split the files (l and r sides should always stay connected!!!) 10-90 using scikit-learn
 # 3. copy the 10 part to a dummy project_repo
