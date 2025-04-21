29d87373fac9566c071f7ca4e41b4b49146f8031
Gál József <h260710@stud.u-szeged.hu>
2025-04-21 23:26:58 +0200

    Fix subprocess for UNIX systems

--- src/facades/pyevolve_facade.py
+++ src/facades/pyevolve_facade.py
@@ -2,7 +2,11 @@ import subprocess
 
 
 def run_pyevolve(root_dir):
-    result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "-h"],
+    # result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "-h"],
+    #                         shell=True,
+    #                         capture_output=True,
+    #                         text=True)
+    result = subprocess.run(f"java -jar {root_dir}/jar/pycraft-1.0-SNAPSHOT.jar -h",
                             shell=True,
                             capture_output=True,
                             text=True)
@@ -10,8 +14,12 @@ def run_pyevolve(root_dir):
 
 
 def run_pyevolve_infer(root_dir, patterns, rules):
-    result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "infer",
-                             "-p", patterns, "-r", rules],
+    # result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "infer",
+    #                          "-p", patterns, "-r", rules],
+    #                         shell=True,
+    #                         capture_output=True,
+    #                         text=True)
+    result = subprocess.run(f"java -jar {root_dir}/jar/pycraft-1.0-SNAPSHOT.jar infer -p {patterns} -r {rules}",
                             shell=True,
                             capture_output=True,
                             text=True)
@@ -19,11 +27,16 @@ def run_pyevolve_infer(root_dir, patterns, rules):
 
 
 def run_pyevolve_transform(root_dir, repositories, types, files, patterns):
-    result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "transform",
-                             "-r", repositories,
-                             "-t", types,
-                             "-f", files,
-                             "-p", patterns],
+    # result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "transform",
+    #                          "-r", repositories,
+    #                          "-t", types,
+    #                          "-f", files,
+    #                          "-p", patterns],
+    #                         shell=True,
+    #                         capture_output=True,
+    #                         text=True)
+    result = subprocess.run((f"java -jar {root_dir}/jar/pycraft-1.0-SNAPSHOT.jar transform"
+                             + f" -r {repositories} -t {types} -f {files} -p {patterns}"),
                             shell=True,
                             capture_output=True,
                             text=True)
