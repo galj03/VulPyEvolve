22bb89135d3ce5c56e09abfdf0d1c991ba694c35
Gál József <h260710@stud.u-szeged.hu>
2025-05-17 09:47:36 +0200

    More cleanup

--- src/facades/pyevolve_facade.py
+++ src/facades/pyevolve_facade.py
@@ -2,10 +2,6 @@ import subprocess
 
 
 def run_pyevolve(root_dir):
-    # result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "-h"],
-    #                         shell=True,
-    #                         capture_output=True,
-    #                         text=True)
     result = subprocess.run(f"java -jar {root_dir}/jar/pycraft-1.0-SNAPSHOT.jar -h",
                             shell=True,
                             capture_output=True,
@@ -14,11 +10,6 @@ def run_pyevolve(root_dir):
 
 
 def run_pyevolve_infer(root_dir, patterns, rules):
-    # result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "infer",
-    #                          "-p", patterns, "-r", rules],
-    #                         shell=True,
-    #                         capture_output=True,
-    #                         text=True)
     result = subprocess.run(f"java -jar {root_dir}/jar/pycraft-1.0-SNAPSHOT.jar infer -p {patterns} -r {rules}",
                             shell=True,
                             capture_output=True,
@@ -27,14 +18,6 @@ def run_pyevolve_infer(root_dir, patterns, rules):
 
 
 def run_pyevolve_transform(root_dir, repositories, types, files, patterns):
-    # result = subprocess.run(["java", "-jar", f"{root_dir}/jar/pycraft-1.0-SNAPSHOT.jar", "transform",
-    #                          "-r", repositories,
-    #                          "-t", types,
-    #                          "-f", files,
-    #                          "-p", patterns],
-    #                         shell=True,
-    #                         capture_output=True,
-    #                         text=True)
     result = subprocess.run((f"java -jar {root_dir}/jar/pycraft-1.0-SNAPSHOT.jar transform"
                              + f" -r {repositories} -t {types} -f {files} -p {patterns}"),
                             shell=True,
